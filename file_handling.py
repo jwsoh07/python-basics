41c379187d087c7d0a9df1c7b913253574c8755a
Soh Jun Wen <mail.junwen@gmail.com>
2025-02-18 15:18:18 +0800

    Added file reading functionality

--- file_handling.py
+++ file_handling.py
@@ -8,3 +8,8 @@ with open("example.txt", "a") as file:
     file.write("\nAppending some text here!")  # \n ensures it goes to a new line
 
 print("File written successfully!")
+
+# Read contents from the file in read mode ('r')
+with open("example.txt", "r") as file:
+    content = file.read()  # Read the entire file content
+    print("File contents:\n", content)
