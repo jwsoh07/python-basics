8bddaf6b3a377a5b884005de481be4b2ef39ceda
Soh Jun Wen <mail.junwen@gmail.com>
2025-02-18 15:13:17 +0800

    Updated file handling script to append text

--- file_handling.py
+++ file_handling.py
@@ -3,4 +3,8 @@ with open("example.txt", "w") as file:
   file.write("Hello, world!\n")
   file.write("This is a file handling exercise.\n")
 
+# Open a file in append mode ('a')
+with open("example.txt", "a") as file:
+    file.write("\nAppending some text here!")  # \n ensures it goes to a new line
+
 print("File written successfully!")
