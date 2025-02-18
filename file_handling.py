f1879b9b9dd36d37bbada43bd98286e0615a4057
Soh Jun Wen <mail.junwen@gmail.com>
2025-02-18 14:57:04 +0800

    Initial commit - File handling basics

---
+++ file_handling.py
@@ -0,0 +1,6 @@
+# Open a file in write mode ('w)
+with open("example.txt", "w") as file:
+  file.write("Hello, world!\n")
+  file.write("This is a file handling exercise.\n")
+
+print("File written successfully!")
